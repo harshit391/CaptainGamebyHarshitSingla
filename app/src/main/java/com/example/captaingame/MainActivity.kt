6846dfaf4df172362dfd46c9165d286b2eeb0b46
harshit391 <harshitsingla1761@gmail.com>
2023-12-31 21:02:00 +0530

    Basic Version is Ready

--- app/src/main/java/com/example/captaingame/MainActivity.kt
+++ app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.captaingame
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -28,13 +26,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.captaingame.ui.theme.CaptainGameTheme
 import kotlin.random.Random
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
@@ -86,16 +82,16 @@ class MainActivity : ComponentActivity() {
             }
         } else {
             when (difficulty) {
-                1 -> CaptainGame(10000)
-                2 -> CaptainGame(10000)
-                3 -> CaptainGame(10000)
+                1 -> CaptainGame(10000,500,1500)
+                2 -> CaptainGame(10000,2000,3000)
+                3 -> CaptainGame(10000,3500,4500)
                 else -> {}
             }
         }
     }
 
     @Composable
-    fun Result(found: Int) {
+    fun Result(found: Int, diff: Int) {
         var reset by remember {
             mutableStateOf(false)
         }
@@ -103,6 +99,18 @@ class MainActivity : ComponentActivity() {
         val myStyle = TextStyle(
             fontSize = 40.sp,
         )
+
+        var difficult by remember {
+            mutableStateOf("Easy")
+        }
+
+        if (diff == 2000) {
+            difficult = "Medium"
+        }
+
+        if (diff == 3500) {
+            difficult = "Hard"
+        }
 
         if (!reset) {
             Box(
@@ -124,13 +132,20 @@ class MainActivity : ComponentActivity() {
                     )
                     Text(
                         modifier = Modifier.padding(vertical = 32.dp),
+                        text = "Game Difficulty :- $difficult",
+                        color = Color.Green,
+                        textAlign = TextAlign.Center,
+                        style = myStyle
+                    )
+                    Text(
+                        modifier = Modifier.padding(vertical = 32.dp),
                         text = "Treasure Found: $found",
                         color = Color.Yellow,
                         textAlign = TextAlign.Center,
                         style = myStyle
                     )
-                    Button(onClick = { reset = true }) {
-                        Text("Reset")
+                    Button(onClick = { reset = true }, modifier = Modifier.height(64.dp).width(120.dp)) {
+                        Text("Reset", fontSize = 24.sp)
                     }
                 }
             }
@@ -140,9 +155,9 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun CaptainGame(Xp: Int) {
+    fun CaptainGame(xP: Int, fromRange: Int, toRange: Int) {
         var shipXP by remember {
-            mutableIntStateOf(Xp)
+            mutableIntStateOf(xP)
         }
 
         var treasuresFound by remember {
@@ -174,12 +189,12 @@ class MainActivity : ComponentActivity() {
 
                 Button(onClick = {
                     direction.value = "East"
-                    val generate = Random.nextInt()
-                    if (generate % 2 == 0) {
+                    val generate = Random.nextInt(fromRange,toRange)
+                    if (generate%2==0) {
                         treasuresFound += 1
                         stormOrTreasure.value = "Found a Treasure!"
                     } else {
-                        val destroyed = Random.nextInt(500, 1000)
+                        val destroyed = Random.nextInt(fromRange, toRange)
                         shipXP -= destroyed
                         stormOrTreasure.value = "Storm Ahead!"
                     }
@@ -189,11 +204,12 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
                     direction.value = "West"
-                    if (Random.nextBoolean()) {
+                    val generate = Random.nextInt(fromRange,toRange)
+                    if (generate%2==0) {
                         treasuresFound += 1
                         stormOrTreasure.value = "Found a Treasure!"
                     } else {
-                        val destroyed = Random.nextInt(500, 2000)
+                        val destroyed = Random.nextInt(fromRange, toRange)
                         shipXP -= destroyed
                         stormOrTreasure.value = "Storm Ahead!"
                     }
@@ -203,11 +219,12 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
                     direction.value = "South"
-                    if (Random.nextBoolean()) {
+                    val generate = Random.nextInt(fromRange,toRange)
+                    if (generate%2==0) {
                         treasuresFound += 1
                         stormOrTreasure.value = "Found a Treasure!"
                     } else {
-                        val destroyed = Random.nextInt(500, 2000)
+                        val destroyed = Random.nextInt(fromRange, toRange)
                         shipXP -= destroyed
                         stormOrTreasure.value = "Storm Ahead!"
                     }
@@ -217,11 +234,12 @@ class MainActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(onClick = {
                     direction.value = "North"
-                    if (Random.nextBoolean()) {
+                    val generate = Random.nextInt(fromRange,toRange)
+                    if (generate%2==0) {
                         treasuresFound += 1
                         stormOrTreasure.value = "Found a Treasure!"
                     } else {
-                        val destroyed = Random.nextInt(500, 2000)
+                        val destroyed = Random.nextInt(fromRange, toRange)
                         shipXP -= destroyed
                         stormOrTreasure.value = "Storm Ahead!"
                     }
@@ -229,7 +247,7 @@ class MainActivity : ComponentActivity() {
                     Text("Sail North")
                 }
             } else {
-                Result(treasuresFound)
+                Result(treasuresFound,fromRange)
             }
         }
     }
