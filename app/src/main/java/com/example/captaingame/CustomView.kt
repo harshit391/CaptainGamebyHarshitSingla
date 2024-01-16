87c7200aa8b21ab21ea2dbef7d575c4a3984b838
Harshit Singla <harshitsingla1761@gmail.com>
2024-01-16 10:59:08 +0530

    Counter View Model Added

---
+++ app/src/main/java/com/example/captaingame/CustomView.kt
@@ -0,0 +1,128 @@
+package com.example.captaingame
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CustomView : ViewModel() {
+
+    private val _repository : Repo = Repo();
+
+    private val _difficulty = mutableIntStateOf(_repository.getCaptainGameData().difficulty)
+    private val _reset = mutableStateOf(_repository.getCaptainGameData().reset)
+    private val _difficult = mutableStateOf(_repository.getCaptainGameData().difficult);
+    private val _shipXP = mutableIntStateOf(_repository.getCaptainGameData().shipXP)
+
+    private val _treasureFound = mutableIntStateOf(_repository.getCaptainGameData().treasureFound)
+    private val _direction = mutableStateOf(_repository.getCaptainGameData().direction)
+    private val _stormOrTreasure = mutableStateOf(_repository.getCaptainGameData().stormOrTreasure)
+
+    val difficultyPublic : MutableState<Int> = _difficulty
+    val resetPublic : MutableState<Boolean> = _reset
+    val difficultPublic : MutableState<String> = _difficult
+    val shipXpPublic : MutableState<Int> = _shipXP
+
+    val treasureFoundPublic : MutableState<Int> = _treasureFound
+    val directionPublic : MutableState<String> = _direction
+    val stormOrTreasurePublic : MutableState<String> = _stormOrTreasure
+
+    fun resetEverything() {
+        _repository.resetEverything()
+        _difficulty.intValue = _repository.getCaptainGameData().difficulty
+        _reset.value = _repository.getCaptainGameData().reset
+        _difficult.value = _repository.getCaptainGameData().difficult
+        _shipXP.intValue = _repository.getCaptainGameData().shipXP
+        _treasureFound.intValue = _repository.getCaptainGameData().treasureFound
+        _direction.value = _repository.getCaptainGameData().direction
+        _stormOrTreasure.value = _repository.getCaptainGameData().stormOrTreasure
+    }
+    fun setDifficulty1() {
+        _repository.setDifficulty1()
+        _difficulty.intValue = _repository.getCaptainGameData().difficulty
+    }
+
+    fun setDifficulty2() {
+        _repository.setDifficulty2()
+        _difficulty.intValue = _repository.getCaptainGameData().difficulty
+    }
+
+    fun setDifficulty3() {
+        _repository.setDifficulty3()
+        _difficulty.intValue = _repository.getCaptainGameData().difficulty
+    }
+
+    fun resetDifficulty() {
+        _repository.resetDifficulty()
+        _difficulty.intValue = _repository.getCaptainGameData().difficulty
+    }
+
+    fun resetIt() {
+        _repository.resetIt()
+        _reset.value = _repository.getCaptainGameData().reset
+    }
+
+    fun unResetIt() {
+        _repository.resetIt()
+        _reset.value = _repository.getCaptainGameData().reset
+    }
+
+    fun setDifficult1() {
+        _repository.setDifficult1()
+        _difficult.value = _repository.getCaptainGameData().difficult
+    }
+
+    fun setDifficult2() {
+        _repository.setDifficult2()
+        _difficult.value = _repository.getCaptainGameData().difficult
+    }
+
+    fun setDifficult3() {
+        _repository.setDifficult3()
+        _difficult.value = _repository.getCaptainGameData().difficult
+    }
+
+    fun decreaseShipXp(value: Int) {
+        _repository.decreaseShipXp(value)
+        _shipXP.intValue = _repository.getCaptainGameData().shipXP
+    }
+
+    fun foundTheTreasure() {
+        _repository.foundTheTreasure()
+        _treasureFound.intValue = _repository.getCaptainGameData().treasureFound
+    }
+
+    fun setDirectionNorth() {
+        _repository.setDirectionNorth()
+        _direction.value = _repository.getCaptainGameData().direction
+    }
+
+    fun setDirectionEast() {
+        _repository.setDirectionEast()
+        _direction.value = _repository.getCaptainGameData().direction
+    }
+
+    fun setDirectionSouth() {
+        _repository.setDirectionSouth()
+        _direction.value = _repository.getCaptainGameData().direction
+    }
+
+    fun setDirectionWest() {
+        _repository.setDirectionWest()
+        _direction.value = _repository.getCaptainGameData().direction
+    }
+
+    fun storm() {
+        _repository.storm()
+        _stormOrTreasure.value = _repository.getCaptainGameData().stormOrTreasure
+    }
+
+    fun treasure() {
+        _repository.treasure()
+        _stormOrTreasure.value = _repository.getCaptainGameData().stormOrTreasure
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/captaingame/dataClasses.kt
@@ -0,0 +1,90 @@
+package com.example.captaingame
+
+data class CaptainGameData(var difficulty:Int, var reset: Boolean, var difficult:String, var shipXP:Int,
+    var treasureFound: Int, var direction:String, var stormOrTreasure:String)
+
+class Repo{
+    private var _start = CaptainGameData(
+-1,false, "Easy",10000,0,"North",""
+    )
+
+    fun getCaptainGameData() = _start
+
+    fun resetEverything() {
+        _start.difficulty = -1
+        _start.reset = false
+        _start.difficult = "Easy"
+        _start.shipXP = 10000
+        _start.treasureFound = 0
+        _start.direction = "North"
+        _start.stormOrTreasure = ""
+    }
+
+    fun setDifficulty1() {
+        _start.difficulty = 1
+    }
+
+    fun setDifficulty2() {
+        _start.difficulty = 2
+    }
+
+    fun setDifficulty3() {
+        _start.difficulty = 3
+    }
+
+    fun resetDifficulty() {
+        _start.difficulty = -1
+    }
+
+    fun resetIt() {
+        _start.reset = true
+    }
+
+    fun unResetIt() {
+        _start.reset = false
+    }
+
+    fun setDifficult1() {
+        _start.difficult = "Easy"
+    }
+
+    fun setDifficult2() {
+        _start.difficult = "Medium"
+    }
+
+    fun setDifficult3() {
+        _start.difficult = "Hard"
+    }
+
+    fun decreaseShipXp(value: Int) {
+        _start.shipXP -= value
+    }
+
+    fun foundTheTreasure() {
+        _start.treasureFound++
+    }
+
+    fun setDirectionNorth() {
+        _start.direction = "North"
+    }
+
+    fun setDirectionEast() {
+        _start.direction = "East"
+    }
+
+    fun setDirectionSouth() {
+        _start.direction = "South"
+    }
+
+    fun setDirectionWest() {
+        _start.direction = "West"
+    }
+
+    fun storm() {
+        _start.stormOrTreasure = "Storm Ahead !!"
+    }
+
+    fun treasure() {
+        _start.stormOrTreasure = "Treasure Found !!"
+    }
+}
